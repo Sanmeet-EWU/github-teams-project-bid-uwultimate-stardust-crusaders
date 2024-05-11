bed421bec5a94266a8e4337a70e20388d58b958f
lthomas32 <lthomas32@ewu.edu>
2024-05-10 21:56:50 -0700

    adding a method to machine for the topology to generate reports

--- src/Machine.py
+++ src/Machine.py
@@ -6,6 +6,14 @@ class Machine:
         self.IP = ipaddress.ip_address(IP) if IP else None
         self.OS = OS
         self.vulnerabilities = []
-
+        self.security_rating = 0
+        
     def add_vulnerability(self, vulnerability):
         self.vulnerabilities.append(vulnerability)
+
+    def generate_report(self) -> str:
+    '''
+    I need some vulnerability report here to return a string and rating. 
+    This will be called when you click on the map in network topology.
+    '''
+        return "This machine is vulnerable"
