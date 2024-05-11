52bcf3a6528d4da214113e18a0d70c6bc7c809fc
lthomas32 <lthomas32@ewu.edu>
2024-05-11 11:51:54 -0700

    Adding comments for future work to implement and understand how the rough draft works

--- src/networkgui.py
+++ src/networkgui.py
@@ -6,29 +6,51 @@ from PyQt6.QtGui import QBrush, QColor, QPen
 
 
 class GraphNode(QGraphicsEllipseItem):
+    ''' TODO
+    1. add machine in as an input to associate it to a note
+    2. set colors based off machine value.
+    3. set labels based on machine tostring
+    '''
     def __init__(self, label, x, y):
         super().__init__(QRectF(x - 55, y - 55, 110, 110))  # Center the ellipse on the coordinates
+        # Need to edit this to reflect the hover menu
         self.label = label
         self.setBrush(QBrush(QColor(138, 3, 3)))
         self.setFlags(QGraphicsEllipseItem.GraphicsItemFlag.ItemIsSelectable |
                         QGraphicsEllipseItem.GraphicsItemFlag.ItemIsFocusable)
         self.setAcceptHoverEvents(True)
 
+        
     def hoverEnterEvent(self, event):
-        print(f"Hovered over Node {self.label}")
+        ''' 
+        Function to show label over node
+        '''
         QToolTip.showText(event.screenPos(), self.label)
         super().hoverEnterEvent(event)
 
     def hoverLeaveEvent(self, event):
+        '''
+        Clean up function to remove label
+        '''
         QToolTip.hideText()
         super().hoverLeaveEvent(event)
 
 
     def mousePressEvent(self, event):
+        '''TODO
+        Link generate report to this button
+        Do we make a pop up what we do?
+        '''
         print(f"Node {self.label} clicked")
         super().mousePressEvent(event)
 
 class GraphWindow(QWidget):
+    '''
+    This made need to be edited to incorporate into main page.
+    Or possible pop up?
+    Automatic on page layout?
+    '''
+
     def __init__(self):
         super().__init__()
 
@@ -40,30 +62,40 @@ class GraphWindow(QWidget):
         self.graphicsView.setScene(self.scene)
         self.scene.setSceneRect(-800, -800, 1600, 1600)  # Adjusted for potentially larger graph
 
-        # Initialize and display graph
-        self.display_graph(20)  # Modify the number of nodes as needed
+        # Displays the graph. Function takes the node.
+        # Maybe the topology class should be passed in?
+        self.display_graph(20)
 
     def display_graph(self, num_nodes):
-        # Create a graph
+        ''' TODO
+        1. Incorporate machines in here.
+        2. do we need a get from the topology class
+        '''
+
         nxG = nx.complete_graph(num_nodes)
-        pos = nx.spring_layout(nxG, scale=800)  # Layout positions
+        # Edit scale for spacing of buttons
+        pos = nx.spring_layout(nxG, scale=600)
 
         # Create nodes and add to scene
         for edge in nxG.edges:
             start_pos = QPointF(*pos[edge[0]])
             end_pos = QPointF(*pos[edge[1]])
             line = QGraphicsLineItem(start_pos.x(), start_pos.y(), end_pos.x(), end_pos.y())
-            line.setPen(QPen(QColor(50, 50, 50), 2))  # Set edge color and thickness
+            # Set edge color and thickness
+            line.setPen(QPen(QColor(50, 50, 50), 2))  
             self.scene.addItem(line)
 
         for node in nxG.nodes:
+            # Get coordinates of the graph
             x, y = pos[node]
-            node_item = GraphNode(f"127.0.0.{node + 1}", x, y)  # Creating GraphNode with IP-like label
+            # Need to add the machine in this constructor
+            node_item = GraphNode(f"127.0.0.{node + 1}", x, y)  
             self.scene.addItem(node_item)
 
 
 def main():
     app = QApplication(sys.argv)
+    # Random css for the hover events
     app.setStyleSheet("""
     QToolTip {
         background-color: #2a82da; /* Light blue background */
