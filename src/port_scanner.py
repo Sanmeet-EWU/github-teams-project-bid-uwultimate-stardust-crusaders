1c85fdaa3516419aeb05f2bb5e3d78c82da90b83
William Reese <wreese1@ewu.edu>
2024-05-25 15:48:51 -0700

    Start demo for CPE API.

--- src/port_scanner.py
+++ src/port_scanner.py
@@ -1,18 +1,27 @@
 """Module containing port scanning related classes and methods."""
 
-import subprocess
 import re
 import json
 from datetime import datetime
+from enum import Enum, auto
 from typing import TypedDict
 
 import nmap
+
+from vulnerability_lookup.cpe import CPE
+
+class PortState(Enum):
+    """Enum representing the state of a port."""
+    OPEN = auto()
+    CLOSED = auto()
+    FILTERED = auto()
 
 
 class NmapServiceData(TypedDict):
     """Type for parsed nmap data for individual services."""
     cpe: str
-    title: str
+    name: str
+    state: str
 
 
 class NmapOSData(TypedDict):
@@ -52,26 +61,26 @@ class PortScanner:
             else:
                 raise ValueError("Invalid port range format")
 
-    def scan_ports(self, port_range=None):
+    def scan_ports(self) -> tuple[tuple[dict[int, NmapPortData], ...]]:
         if self.host is None:
             raise ValueError("Host not set")
 
-        port_option = self.set_port_range(port_range)
-
         self.raw_output = self.scanner.scan(
-            hosts=self.host,
-            ports='1-1000',
-            arguments='-sS -sU -A -O -sV')
+            hosts="scanme.nmap.org",
+            arguments='-sV -T4 --open -O')
 
-        return self.raw_output
+        return tuple((
+            self._parse_scan_host(host)
+            for host in self.raw_output['scan'].values()
+        ))
 
-    def _parse_scan_data(
+    def _parse_scan_host(
             self,
-            raw_data: dict) -> dict[int, NmapPortData]:
-        """Parse the raw scan data into a usable form.
+            host_data: dict) -> tuple[dict[int, NmapPortData], ...]:
+        """Parse the raw scan data for a host into a usable form.
 
         Args:
-            raw_data (object):
+            host_data (object):
                 A JSON dictionary containing the parsed data
                 from a nmap scan.
 
@@ -80,34 +89,73 @@ class PortScanner:
                 A dictionary containing relevant nmap data for
                 each every port on a given host.
         """
-        
-        return None
+        # This is bad I just want to get this done. TODO come back later...
+        tcp_data = {}
+        if host_data.get('tcp'):
+            for port, port_data in host_data['tcp'].items():
+                tcp_data[int(port)] = {
+                    'cpe': port_data['cpe'],
+                    'name': port_data['name'],
+                    'state': (
+                        PortState.OPEN if port_data['state'] == 'open' else
+                        PortState.CLOSED if port_data['state'] == 'closed' else
+                        PortState.FILTERED
+                    )
+                }
+        udp_data = {}
+        if host_data.get('udp'):
+            for port, port_data in host_data['udp'].items():
+                udp_data[int(port)] = {
+                    'cpe': port_data['cpe'],
+                    'name': port_data['name'],
+                    'state': (
+                        PortState.OPEN if port_data['state'] == 'open' else
+                        PortState.CLOSED if port_data['state'] == 'closed' else
+                        PortState.FILTERED
+                    )
+                }
+        os_data = {}
+        if host_data.get('osmatch'):
+            for os in host_data['osmatch']:
+                os_name = os['name']
+                os_data[os_name] = {
+                    'cpe': os['osclass'][0]['cpe'][0],
+                    'name': os_name
+                }
 
-    def save_scan_data(self):
-        file_name = f"nmap_scan_{self.host}_{datetime.now().strftime('%Y%m%d_%H%M%S')}.json"
-        with open(file_name, 'w') as f:
-            f.write(self.raw_output)
-        print(f"Scan data saved to {file_name}")
+        return tcp_data, os_data
 
-    def print_scan_data(self):
-        if self.raw_output:
-            print("Scan Results:")
-            print(json.dumps(self.raw_output.splitlines(), indent=4))
-            self.save_scan_data()
-        else:
-            print("No scan data available")
 
 def main():
     scanner = PortScanner()
-    scanner.set_host("127.0.0.1")
-    #demo main for testing purposes- hard coded for local host
-    #right now it prints the whole scan output. next thing is formatting & parsing it to make it look pretty and extract/save info for cve stuff
+    scanner.set_host("")
 
-    port_range = input("Enter port range (e.g., '1-100', 'all') or press Enter to scan the 1000 most common ports: ") or None
-
-    scan_results = scanner.scan_ports(port_range)
-    if scan_results:
-        scanner.print_scan_data()
+    scan_results: tuple[tuple[dict[int, NmapPortData], ...]] = scanner.scan_ports()
+    found_with_cpe = 0
+    port_cpes: list[tuple[int, CPE]] = []
+    os_cpes: list[tuple[str, CPE]] = []
+    total_found = 0
+    for host_data in scan_results:
+        tcp_data = host_data[0]
+        os_data = host_data[1]
+        for port, port_data in tcp_data.items():
+            print(port, port_data['name'], port_data['cpe'])
+            if port_data['cpe']:
+                port_cpes.append((port, CPE.create_from_str(port_data['cpe'])))
+                found_with_cpe += 1
+            total_found += 1
+        for os, os_data in os_data.items():
+            print(os, os_data['name'], os_data['cpe'])
+            if os_data['cpe']:
+                os_cpes.append((os, CPE.create_from_str(os_data['cpe'])))
+                found_with_cpe += 1
+            total_found += 1
+    if total_found > 0:
+        for port, cpe in port_cpes:
+            print(f"port:\n {port}", json.dumps(cpe.find_related_cves(), indent=4))
+        for os, cpe in os_cpes:
+            print(f"os:\n {os}", json.dumps(cpe.find_related_cves(), indent=4))
+        print(f"Certainty percentage: {found_with_cpe / total_found * 100}%")
     else:
         print("No results found.")
 
