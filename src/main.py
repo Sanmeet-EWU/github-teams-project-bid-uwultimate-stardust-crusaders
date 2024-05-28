b011bb32d18d7487892d9938cbd8bd0de9145c18
alexa darrington <96143366+alyxadarr@users.noreply.github.com>
2024-05-27 19:30:35 -0700

    moved home button to bottom

--- src/main.py
+++ src/main.py
@@ -16,12 +16,6 @@ class MainInterface(QWidget):
         self.stacked_widget = stacked_widget
         main_layout = QVBoxLayout()
 
-        back_button = QPushButton("Back to Home")
-        back_button.setMinimumSize(60, 25)
-        back_button.setStyleSheet("font-size: 12px; padding: 7px 15px;")
-        back_button.clicked.connect(self.go_to_welcome_page)
-        main_layout.addWidget(back_button)
-
         # Tabs
         tab_widget = QTabWidget()
         main_layout.addWidget(tab_widget)
@@ -37,17 +31,14 @@ class MainInterface(QWidget):
         scan_layout.addWidget(self.scan_options_widget)
         tab_widget.addTab(scan_tab, "Scan Subnets")
 
-
         # Ports Tab
-        port_tab= QWidget()
+        port_tab = QWidget()
         port_layout = QVBoxLayout()
         self.port_options_widget = PortOptionsWidget(self.network)
         port_layout.addWidget(self.port_options_widget)
         port_tab.setLayout(port_layout)
 
         tab_widget.addTab(port_tab, "Scan Ports")
-
-
 
         # Display Topology Tab
         display_topology_tab = QWidget()
@@ -69,11 +60,26 @@ class MainInterface(QWidget):
         # exploit_layout.addWidget(QLabel("Menu for Exploit Options ex Default Credentials"))
         # tab_widget.addTab(exploit_tab, "Exploit")
 
+        main_layout.addStretch()
+
+        back_button = QPushButton("Back Home")
+        back_button.setMinimumSize(100, 40)
+        back_button.setStyleSheet("""
+            font-size: 14px;
+            font-weight: bold;
+            padding: 7px 15px;
+            border: 2px solid rgb(206, 41, 41); 
+            border-radius: 5px;
+            background-color: #FFFFFF;
+            color: #000000;
+        """)
+        back_button.clicked.connect(self.go_to_welcome_page)
+        main_layout.addWidget(back_button, alignment=Qt.AlignmentFlag.AlignCenter)
+
         self.setLayout(main_layout)
 
     def go_to_welcome_page(self):
         self.stacked_widget.setCurrentIndex(0)
-
 
 class ScanMasterX(QMainWindow):
     def __init__(self):
@@ -81,29 +87,23 @@ class ScanMasterX(QMainWindow):
         self.setWindowTitle("ScanMasterX")
         self.setGeometry(100, 100, 800, 600)
 
-        # Main layout
         main_layout = QVBoxLayout()
 
-        # Stacked widget for switching between welcome page and main interface
         self.stacked_widget = QStackedWidget()
         main_layout.addWidget(self.stacked_widget)
 
-        # Add welcome page and main interface to the stacked widget
         self.stacked_widget.addWidget(WelcomePage(self.stacked_widget))
         self.stacked_widget.addWidget(MainInterface(self.stacked_widget))
 
-        # Set main layout
         container = QWidget()
         container.setLayout(main_layout)
         self.setCentralWidget(container)
-
 
 def main():
     app = QApplication(sys.argv)
     window = ScanMasterX()
     window.show()
     sys.exit(app.exec())
-
 
 if __name__ == '__main__':
     main()
