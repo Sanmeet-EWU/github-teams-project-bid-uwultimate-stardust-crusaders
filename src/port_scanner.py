84b2c9b02364530f2494a0f65bc3472623c71c82
William Reese <wreese1@ewu.edu>
2024-05-28 23:43:03 -0700

    Begin refactor of vulnerability scanner.

--- src/port_scanner.py
+++ src/port_scanner.py
@@ -1,8 +1,6 @@
 """Module containing port scanning related classes and methods."""
 
 import re
-import json
-from datetime import datetime
 from enum import Enum, auto
 from typing import TypedDict
 
@@ -10,29 +8,78 @@ import nmap
 
 from vulnerability_lookup.cpe import CPE
 
+
 class PortState(Enum):
     """Enum representing the state of a port."""
-    OPEN = auto()
-    CLOSED = auto()
-    FILTERED = auto()
+    OPEN = 'open'
+    CLOSED = 'closed'
+    FILTERED = 'filtered'
+    UNKNOWN = auto()
 
 
-class NmapServiceData(TypedDict):
-    """Type for parsed nmap data for individual services."""
-    cpe: str
+class NmapPortData(TypedDict):
+    """Type for parsed nmap data for individual ports."""
+    cpe: CPE | None
     name: str
     state: str
 
 
 class NmapOSData(TypedDict):
     """Type for parsed nmap data for an OS."""
-    cpe: str
+    cpe: CPE | None
+    name: str
 
 
-class NmapPortData(TypedDict):
-    """Type for parsed nmap data for individual ports."""
-    services: tuple[NmapServiceData]
-    os: NmapOSData
+class ParsedNmapData(TypedDict):
+    tcp: dict[int, NmapPortData]
+    udp: dict[int, NmapPortData]
+    os: NmapOSData | None
+
+
+class NmapDataParser:
+    def __init__(self, data: object):
+        self._data = data
+
+    def parse(self) -> ParsedNmapData:
+        return {
+            'tcp': self._parse_protocol(self._data['scan'], 'tcp'),
+            'udp': self._parse_protocol(self._data['scan'], 'udp'),
+            'os': self._parse_os(self._data['scan']),
+        }
+
+    def _parse_protocol(
+            self,
+            data: object,
+            protocol: str) -> dict[int, NmapPortData]:
+        results = {}
+        if protocol_data := data.get(protocol):
+            for port, port_data in protocol_data.items():
+                results[int(port)] = {
+                    'cpe': CPE.create_from_str(port_data['cpe']),
+                    'name': port_data['name'],
+                    'state': (
+                        PortState(port_data['state']) if
+                        port_data['state'] in (
+                            state.value for state in PortState) else
+                        PortState.UNKNOWN
+                    )
+                }
+        return results
+
+    def _parse_os(self, data: object) -> dict[str, NmapOSData]:
+        results = None
+        if os_data := data.get('osmatch'):
+            if first_os := os_data[0]:
+                os_name = first_os['name']
+                cpe = ""
+                if len(os_class := first_os['osclass']) > 0:
+                    if len(os_cpes := os_class[0].get('cpe')) > 0:
+                        cpe = os_cpes[0]
+                os_data[os_name] = {
+                    'cpe': CPE.create_from_str(cpe),
+                    'name': os_name
+                }
+        return results
 
 
 class PortScanner:
@@ -46,118 +93,36 @@ class PortScanner:
     def set_host(self, host):
         self.host = host
 
-    def set_port_range(self, port_range=None):
-        if port_range is None:
-            return ""  # default to 1000 most common
-        elif re.match(r"all", port_range, re.IGNORECASE):
-            return "-p-"
-        else:
-            match = re.match(r"(\d+)\s*-\s*(\d+)", port_range)
-            if match:
-                start_port, end_port = map(int, match.groups())
-                if start_port < 1 or end_port > 65535 or start_port > end_port:
-                    raise ValueError("Invalid port range")
-                return f"-p {start_port}-{end_port}"
-            else:
-                raise ValueError("Invalid port range format")
-
-    def scan_ports(self) -> tuple[tuple[dict[int, NmapPortData], ...]]:
+    def scan_ports(
+            self,
+            port_start: int = 0,
+            port_end: int = 65355) -> ParsedNmapData:
         if self.host is None:
             raise ValueError("Host not set")
-
-        self.raw_output = self.scanner.scan(
-            hosts="scanme.nmap.org",
-            arguments='-sV -T4 --open -O')
-
-        return tuple((
-            self._parse_scan_host(host)
-            for host in self.raw_output['scan'].values()
-        ))
-
-    def _parse_scan_host(
-            self,
-            host_data: dict) -> tuple[dict[int, NmapPortData], ...]:
-        """Parse the raw scan data for a host into a usable form.
-
-        Args:
-            host_data (object):
-                A JSON dictionary containing the parsed data
-                from a nmap scan.
-
-        Returns:
-            dict[int, NmapPortData]]:
-                A dictionary containing relevant nmap data for
-                each every port on a given host.
-        """
-        # This is bad I just want to get this done. TODO come back later...
-        tcp_data = {}
-        if host_data.get('tcp'):
-            for port, port_data in host_data['tcp'].items():
-                tcp_data[int(port)] = {
-                    'cpe': port_data['cpe'],
-                    'name': port_data['name'],
-                    'state': (
-                        PortState.OPEN if port_data['state'] == 'open' else
-                        PortState.CLOSED if port_data['state'] == 'closed' else
-                        PortState.FILTERED
-                    )
-                }
-        udp_data = {}
-        if host_data.get('udp'):
-            for port, port_data in host_data['udp'].items():
-                udp_data[int(port)] = {
-                    'cpe': port_data['cpe'],
-                    'name': port_data['name'],
-                    'state': (
-                        PortState.OPEN if port_data['state'] == 'open' else
-                        PortState.CLOSED if port_data['state'] == 'closed' else
-                        PortState.FILTERED
-                    )
-                }
-        os_data = {}
-        if host_data.get('osmatch'):
-            for os in host_data['osmatch']:
-                os_name = os['name']
-                os_data[os_name] = {
-                    'cpe': os['osclass'][0]['cpe'][0],
-                    'name': os_name
-                }
-
-        return tcp_data, os_data
+        return NmapDataParser(
+            self.scanner.scan(
+                hosts="scanme.nmap.org",
+                arguments='-sV -T4 --open -O',
+                ports=f'{port_start}-{port_end}'
+            )
+        ).parse()
 
 
 def main():
     scanner = PortScanner()
     scanner.set_host("")
 
-    scan_results: tuple[tuple[dict[int, NmapPortData], ...]] = scanner.scan_ports()
-    found_with_cpe = 0
-    port_cpes: list[tuple[int, CPE]] = []
-    os_cpes: list[tuple[str, CPE]] = []
-    total_found = 0
-    for host_data in scan_results:
-        tcp_data = host_data[0]
-        os_data = host_data[1]
-        for port, port_data in tcp_data.items():
-            print(port, port_data['name'], port_data['cpe'])
-            if port_data['cpe']:
-                port_cpes.append((port, CPE.create_from_str(port_data['cpe'])))
-                found_with_cpe += 1
-            total_found += 1
-        for os, os_data in os_data.items():
-            print(os, os_data['name'], os_data['cpe'])
-            if os_data['cpe']:
-                os_cpes.append((os, CPE.create_from_str(os_data['cpe'])))
-                found_with_cpe += 1
-            total_found += 1
-    if total_found > 0:
-        for port, cpe in port_cpes:
-            print(f"port:\n {port}", json.dumps(cpe.find_related_cves(), indent=4))
-        for os, cpe in os_cpes:
-            print(f"os:\n {os}", json.dumps(cpe.find_related_cves(), indent=4))
-        print(f"Certainty percentage: {found_with_cpe / total_found * 100}%")
-    else:
-        print("No results found.")
+    scan_results: ParsedNmapData = scanner.scan_ports()
+    for key, data in scan_results.items():
+        if key in ['tcp', 'udp']:
+            for port, port_data in data[key].items():
+                cves = port_data['cve'].find_related_cves(results=100)
+                print(f"Port: {port}, CVEs:\n- " + "\n- ".join(
+                    str(cve) for cve in cves))
+        else:
+            cves = data['cve'].find_related_cves()
+            print(f"Port: {data['name']}, CVEs:\n- " + "\n- ".join(
+                    str(cve) for cve in cves))
 
 
 if __name__ == "__main__":
