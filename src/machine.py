5dac6665457edb0e44c17c02450c9ba95aacbb72
lthomas32 <lthomas32@ewu.edu>
2024-06-04 17:06:35 -0700

    Fixed classes

--- src/machine.py
+++ src/machine.py
@@ -6,8 +6,9 @@ class Machine:
     """"""
     scan_data: ParsedNmapData
 
-    def __init__(self, IP=None):
+    def __init__(self, IP=None, OS=None):
         self.IP = ipaddress.ip_address(IP) if IP else None
+        self.OS = OS
         self.vulnerabilities = []
         self.security_rating = 0
         self.color = (199, 36, 177)
